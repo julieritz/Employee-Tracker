55624a42e117906b93436957d96bd283c7967fc5
julieritz <julie.ritz@gmail.com>
2020-05-22 11:12:22 -0700

    fixing schema

--- db/tracker.sql
+++ db/tracker.sql
@@ -5,24 +5,24 @@ CREATE database tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-  id INTEGER auto_increment NOT NULL,
+  id INT auto_increment NOT NULL,
   name VARCHAR(30)
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INTEGER auto_increment NOT NULL,
+  id INT auto_increment NOT NULL,
   title VARCHAR(30),
   salary DECIMAL(10,3),
-  department_id INTEGER,
+  department_id INT,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INTEGER auto_increment NOT NULL,
+  id INT auto_increment NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER,
+  role_id INT,
+  manager_id INT,
   PRIMARY KEY (id)
 );
